92b90ce8a79b0fcce448068052b67ff93455be42
emon2058 <ashad2058@gmail.com>
2024-12-06 00:30:45 +0600

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 33;
+
+  res.send(`Hello ${a}`);
+});
+
+console.log(process.cwd());
+export default { app };
